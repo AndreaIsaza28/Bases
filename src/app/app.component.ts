4932bcc668936e882a1251de2b8ae8170f40833c
Andrea Isaza <gisaza@playtechla.com>
2023-11-21 16:17:17 -0500

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   public title:string = 'Bienvenidos';
   public counter: number = 10;
